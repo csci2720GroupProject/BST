9fdec5eb7c3c6c3629529c888842c825d14b6d3d
Jakson Hensley <jaksonhensley@gmail.com>
2022-10-06 13:35:29 -0400

    Implemented main() + test cases

--- BST.java
+++ BST.java
@@ -293,4 +293,38 @@ class BST {
 
     } //minumim
 
+    public static void main (String[] args) {
+
+        BST tree = new BST();
+
+        tree.insert(1);
+        tree.preorder();
+
+        tree.delete(1);
+        tree.postorder();
+
+        tree.insert(4);
+        tree.insert(4);
+        tree.insert(5);
+        tree.insert(7);
+        tree.insert(1);
+        tree.insert(9);
+        tree.insert(2);
+        tree.insert(3);
+        tree.insert(6);
+        tree.insert(0);
+        tree.insert(-1);
+
+        tree.preorder();
+        tree.inorder();
+        tree.postorder();
+
+        tree.delete(8);
+        tree.delete(4);
+        tree.delete(6);
+
+        tree.postorder();
+
+    } //main
+
 } //BST
